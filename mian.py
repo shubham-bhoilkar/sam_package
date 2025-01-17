2b639da872505998a870da7525f6f5bc11a5df02
shubham-bhoilkar <shubham.b@neuralit.com>
2025-01-17 18:47:18 +0530

    Update mian.py

--- mian.py
+++ mian.py
@@ -4,7 +4,3 @@ def my_method_demo():
 
 my_method_demo();
 
-inn aakoon ko main kaise najar andaj karu jo motiyoon jaise anmol hai,
-iss chehare ko kaise na neharu, jo chand sa roshan hai,
-iss zhulfeli laharate baloon ko kuy na jhulfau, jo resham se pyare hai,
-yeh sundar aada hi nahi, yeh toh asl main Aapka ka nirmal roop hai! 
